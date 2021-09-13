c913897d5860d7e03e15fcc0b53959297ccd8bc2
shubhampratap1804 <samsunggalaxygrand2pratap@gmail.com>
2021-09-13 23:58:01 +0530

    Added: welcome message

---
+++ src/main/java/com/example/demo/SpringConceptsProjectAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptsProjectAssignmentApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to spring boot first assignment!");
+		SpringApplication.run(SpringConceptsProjectAssignmentApplication.class, args);
+	}
+}
