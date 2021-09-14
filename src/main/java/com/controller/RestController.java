3763f3356a7bc4d644096d769ad9efcf07fd09d5
shubhampratap1804 <samsunggalaxygrand2pratap@gmail.com>
2021-09-14 16:59:22 +0530

    Added: get mapping name as path variable

--- src/main/java/com/controller/RestController.java
+++ src/main/java/com/controller/RestController.java
@@ -1,5 +1,7 @@
 package com.controller;
-import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +16,21 @@ public class RestController {
 		return "Hello, This is Shubham!";
 	}
 	
-	
+	//get request using @GetMapping
 	@GetMapping("/hello")
 	public String helloMessage() {
 		return "Hello From Bridgelabz";
 	}
 	
+	//query parameter using @RequestParam
 	@GetMapping("/name")
 	public String nameAsQueryParameter(@RequestParam String name) {
+		return "My name is: "+name;
+	}
+	
+	 //query parameter using @PathVariables
+	@GetMapping("/name{name}")
+	public String nameAsPathVariable(@PathVariable String name) {
 		return "My name is: "+name;
 	}
 }
