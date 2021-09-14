071b1b4606020e81299b7d578153bf67a261c0ca
shubhampratap1804 <samsunggalaxygrand2pratap@gmail.com>
2021-09-14 16:47:58 +0530

    Added: response and request methods into the controller to get hello message

--- src/main/java/com/controller/RestController.java
+++ src/main/java/com/controller/RestController.java
@@ -1,5 +1,21 @@
 package com.controller;
+import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@org.springframework.web.bind.annotation.RestController
 public class RestController {
-
+	
+	@RequestMapping(value = "/getMessage", method = RequestMethod.GET)
+	@ResponseBody
+	public String getHelloMessage() {
+		return "Hello, This is Shubham!";
+	}
+	
+	
+	@GetMapping("/hello")
+	public String helloMessage() {
+		return "Hello From Bridgelabz";
+	}
 }
