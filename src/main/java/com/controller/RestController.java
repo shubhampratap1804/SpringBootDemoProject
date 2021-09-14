30a834535876810b85ca5c7f7430effcd9855dc7
shubhampratap1804 <samsunggalaxygrand2pratap@gmail.com>
2021-09-14 17:08:05 +0530

    Created: a new pojo class and added post request method to display a message

--- src/main/java/com/controller/RestController.java
+++ src/main/java/com/controller/RestController.java
@@ -1,11 +1,13 @@
 package com.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.bridgelabz.model.ModelClass;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
@@ -16,21 +18,27 @@ public class RestController {
 		return "Hello, This is Shubham!";
 	}
 	
-	//get request using @GetMapping
+	//Get request using @GetMapping
 	@GetMapping("/hello")
 	public String helloMessage() {
 		return "Hello From Bridgelabz";
 	}
 	
-	//query parameter using @RequestParam
+	//Query parameter using @RequestParam
 	@GetMapping("/name")
 	public String nameAsQueryParameter(@RequestParam String name) {
 		return "My name is: "+name;
 	}
 	
-	 //query parameter using @PathVariables
+	 //Query parameter using @PathVariables
 	@GetMapping("/name{name}")
 	public String nameAsPathVariable(@PathVariable String name) {
 		return "My name is: "+name;
 	}
+	
+    //PostMapping with RequestBody
+    @PostMapping("/post")
+    public String postPersonFullName(@RequestBody ModelClass person) {
+        return "Hello, Welcome " + person.getFirstName() + " " + person.getLastName();
+    }
 }
